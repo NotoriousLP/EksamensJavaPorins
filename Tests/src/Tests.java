5cb116b87ed77edb145fbfc72d761a91adbd1271
Lauris P <porinslauris@gmail.com>
2022-06-10 11:20:41 +0300

    Pievienota metode, kurā ir pirmais jautājums

--- Tests/src/Tests.java
+++ Tests/src/Tests.java
@@ -5,9 +5,19 @@ public class Tests {
 	public static void Sakums() {
 		JOptionPane.showMessageDialog(null, "Šis ir elektroniskais tests par Viendimensiju masīviem programmēšanas valodā Java\nTests satur 10 jautājumus, katram jautājumam 4 atbilžu varianti, no kuriem pareizi ir 2 vai 3 atbilžu varianti.");
 	}
+	public static void pirmaisJautajums() {
+		String atbilde;
+		do{
+		atbilde = JOptionPane.showInputDialog("1. Kuri no tiem ir pareizi uzrakstīta masīva sintakse\na) int[] A\nb) int []A\nc) []int A\nd) int A[]\n 2 vai 3 atbildes var būt pareizas\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas tad: a;b\nAtbildes liekat alfabētu secībā!");
+		}while(!atbilde.equals("a;b") && !atbilde.equals("a;c") && !atbilde.equals("a;d") &&  !atbilde.equals("b;c") &&  !atbilde.equals("b;d") &&  !atbilde.equals("c;d") 
+				&&  !atbilde.equals("a;b;c") &&  !atbilde.equals("a;b;d") &&  !atbilde.equals("b;c;d"));
+		if(atbilde.equals("a;b;d")) punkti++;
+	}
+	
+	
 	public static void main(String[] args) {
-	Sakums();
-	 
+	 Sakums();
+	 pirmaisJautajums();
 	}
 
 }
