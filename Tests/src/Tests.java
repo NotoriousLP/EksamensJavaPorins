e315f0c674eeeeb78f9027988234a7837b9b8116
Lauris P <porinslauris@gmail.com>
2022-06-10 11:04:47 +0300

    Pievienota metode Sākums, kur pastāsta, kas ir par testu utt

--- Tests/src/Tests.java
+++ Tests/src/Tests.java
@@ -1,9 +1,13 @@
+import javax.swing.JOptionPane;
 
 public class Tests {
-
+	static int punkti = 0;
+	public static void Sakums() {
+		JOptionPane.showMessageDialog(null, "Šis ir elektroniskais tests par Viendimensiju masīviem programmēšanas valodā Java\nTests satur 10 jautājumus, katram jautājumam 4 atbilžu varianti, no kuriem pareizi ir 2 vai 3 atbilžu varianti.");
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Sakums();
+	 
 	}
 
 }
