03114a72399311166f5fcb886fdc68808eec72ff
Lauris P <porinslauris@gmail.com>
2022-06-13 12:46:06 +0300

    Pielabojumi pie katras metodes, pielikts toLowerCase un pielabots teksts

--- Tests/src/Tests.java
+++ Tests/src/Tests.java
@@ -8,60 +8,91 @@ public class Tests {
 	}
 	public static void pirmaisJautajums() {
 		do{
-		atbilde = JOptionPane.showInputDialog("1. Kuri no tiem ir pareizi uzrakstīta masīva sintakse\na) int[] A\nb) int []A\nc) []int A\nd) int A[]\n 2 vai 3 atbildes var būt pareizas\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas tad: a;b\nAtbildes liekat alfabētu secībā!");
+		atbilde = JOptionPane.showInputDialog("1. Kuri no tiem ir pareizi uzrakstīta masīva sintakse"
+				+ "\na)int[] A"
+				+ "\nb)int []A"
+				+ "\nc)[]int A"
+				+ "\nd)int A[]"
+				+ "\n2 vai 3 atbildes var būt pareizas"
+				+ "\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas tad: a;b"
+				+ "\nAtbildes liekat alfabētu secībā!");
+		atbilde = atbilde.toLowerCase();
 		}while(!atbilde.equals("a;b") && !atbilde.equals("a;c") && !atbilde.equals("a;d") &&  !atbilde.equals("b;c") &&  !atbilde.equals("b;d") &&  !atbilde.equals("c;d") 
 				&&  !atbilde.equals("a;b;c") &&  !atbilde.equals("a;b;d") &&  !atbilde.equals("b;c;d") && !atbilde.equals("a;c;d"));
+		atbilde = atbilde.toLowerCase();
 		if(atbilde.equals("a;b;d")) punkti++;
 	}
 	public static void otraisJautajums() {
 		do {
-			atbilde = JOptionPane.showInputDialog("2. Ar kādiem veidiem var izdrukāt masīvus?\na) for each\nb)do while cikls\nc) while cikls\nd)for cikls\n 2 vai 3 atbildes var būt pareizas\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas tad: a;b\nAtbildes liekat alfabētu secībā!");
+			atbilde = JOptionPane.showInputDialog("2. Ar kādiem veidiem var izdrukāt masīvus?"
+					+ "\na)for each"
+					+ "\nb)do while cikls"
+					+ "\nc)while cikls"
+					+ "\nd)for cikls"
+					+ "\n2 vai 3 atbildes var būt pareizas"
+					+ "\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas atbildes tad: a;b"
+					+ "\nAtbildes liekat alfabētu secībā!");
+			atbilde = atbilde.toLowerCase();
 		}while(!atbilde.equals("a;b") && !atbilde.equals("a;c") && !atbilde.equals("a;d") &&  !atbilde.equals("b;c") &&  !atbilde.equals("b;d") &&  !atbilde.equals("c;d") 
 				&&  !atbilde.equals("a;b;c") &&  !atbilde.equals("a;b;d") &&  !atbilde.equals("b;c;d") && !atbilde.equals("a;c;d"));
 		if(atbilde.equals("a;d")) punkti++;
 	}
 	public static void tresaisJautajums() {
 		do {
-			atbilde = JOptionPane.showInputDialog("3. Kādas ir masīvu priekšrocības?\na)Nav lieluma ierobežojuma\nb)Brīvķluve\nc)Koda optimizācija\nd)Masīva lielums nav fiksēts\n2 vai 3 atbildes var būt pareizas\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas tad: a;b\nAtbildes liekat alfabētu secībā!");
+			atbilde = JOptionPane.showInputDialog("3. Kādas ir masīvu priekšrocības?"
+					+ "\na)Nav lieluma ierobežojuma"
+					+ "\nb)Brīvķluve"
+					+ "\nc)Koda optimizācija"
+					+ "\nd)Masīva lielums nav fiksēts"
+					+ "\n2 vai 3 atbildes var būt pareizas"
+					+ "\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas atbildes tad: a;b"
+					+ "\nAtbildes liekat alfabētu secībā!");
+			atbilde = atbilde.toLowerCase();
 		}while(!atbilde.equals("a;b") && !atbilde.equals("a;c") && !atbilde.equals("a;d") &&  !atbilde.equals("b;c") &&  !atbilde.equals("b;d") &&  !atbilde.equals("c;d") 
 				&&  !atbilde.equals("a;b;c") &&  !atbilde.equals("a;b;d") &&  !atbilde.equals("b;c;d") && !atbilde.equals("a;c;d"));
 		if(atbilde.equals("b;c")) punkti++;
 	}
 	public static void ceturtaisJautajums() {
 		do {
-			atbilde = JOptionPane.showInputDialog("Ja ir dots masīvs A[5], atbildiet, kuri pēc elementu indeksiem nav pareizi"
+			atbilde = JOptionPane.showInputDialog("4.Ja ir dots masīvs A[5], atbildiet, kuri pēc elementu indeksiem nav pareizi"
 					+ "\na)0;1;2;3;4"
 					+ "\nb)0;2;4;6;8"
 					+"\nc)1;2;3;4;5"
 					+ "\nd)4;3;2;1;0"
 					+"\n2 vai 3 atbildes var būt pareizas"
-					+ "\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas tad: a;b"
+					+ "\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas atbildes tad: a;b"
 					+ "\nAtbildes liekat alfabētu secībā!");
+			atbilde = atbilde.toLowerCase();
 		}while(!atbilde.equals("a;b") && !atbilde.equals("a;c") && !atbilde.equals("a;d") &&  !atbilde.equals("b;c") &&  !atbilde.equals("b;d") &&  !atbilde.equals("c;d") 
 				&&  !atbilde.equals("a;b;c") &&  !atbilde.equals("a;b;d") &&  !atbilde.equals("b;c;d") && !atbilde.equals("a;c;d"));
 		if(atbilde.equals("b;c;d")) punkti++;
 	}
 	public static void piektaisJautajums() {
 		do {
-		atbilde = JOptionPane.showInputDialog("Kādas bija viendimensiju masīva kārtošana iebūvētas funkcijas, lai var izdukāt augošā secībā un dilstošā secībā"
+		atbilde = JOptionPane.showInputDialog("5.Kādas bija viendimensiju masīva kārtošana iebūvētas funkcijas, lai var izdukāt augošā secībā un dilstošā secībā"
 					+ "\na)Arrays.sort(masivs);"
 					+ "\nb)Collections.reverseOrder(masivs)"
 					+ "\nc)Sort.arrays(masivs);"
 					+ "\nd)Arrays.sort(masivs, Collections.reverseOrder())"
 					+"\n2 vai 3 atbildes var būt pareizas"
-					+ "\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas tad: a;b"
+					+ "\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas atbildes tad: a;b"
 					+ "\nAtbildes liekat alfabētu secībā!");
+		atbilde = atbilde.toLowerCase();
 		}while(!atbilde.equals("a;b") && !atbilde.equals("a;c") && !atbilde.equals("a;d") &&  !atbilde.equals("b;c") &&  !atbilde.equals("b;d") &&  !atbilde.equals("c;d") 
 				&&  !atbilde.equals("a;b;c") &&  !atbilde.equals("a;b;d") &&  !atbilde.equals("b;c;d") && !atbilde.equals("a;c;d"));
 		if(atbilde.equals("a;d")) punkti++;
 	}
 	public static void sestaisJautajums() {
 		do {
-			atbilde = JOptionPane.showInputDialog("Kura ir pareiza deklerēta masīva sintakse?"
+			atbilde = JOptionPane.showInputDialog("6.Kura ir pareiza deklerēta masīva sintakse?"
 					+ "\na)int[] a = new int[10];"
 					+ "\nb)[]int a = {5,10,20,21};"
 					+ "\nc)int[] a = new double[14];"
-					+ "\nd)int a[] = {15,20,30,40};");
+					+ "\nd)int a[] = {15,20,30,40};"
+					+"\n2 vai 3 atbildes var būt pareizas"
+					+ "\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas atbildes tad: a;b"
+					+ "\nAtbildes liekat alfabētu secībā!");
+			atbilde = atbilde.toLowerCase();
 		}while(!atbilde.equals("a;b") && !atbilde.equals("a;c") && !atbilde.equals("a;d") &&  !atbilde.equals("b;c") &&  !atbilde.equals("b;d") &&  !atbilde.equals("c;d") 
 				&&  !atbilde.equals("a;b;c") &&  !atbilde.equals("a;b;d") &&  !atbilde.equals("b;c;d") && !atbilde.equals("a;c;d"));
 		if(atbilde.equals("a;d")) punkti++;
@@ -72,21 +103,26 @@ public class Tests {
 				+ "\na)Salīdzinot pirmo elementu ar meklējamo vērtību, ja nav tad turpina pie otrā."
 				+ "\nb)Sāk meklēt ciparu, kuru vēlaties no elementu beigām līdz elementu sākumam."
 				+ "\nc)Masīvs tiek sadalīts uz pusēm lai var sākt meklēt vērtības, kuras vēlaties."
-				+ "\nd)Masīvs tiek šķērsots secīgi un tiek pārbaudīts katrs elements.");
+				+ "\nd)Masīvs tiek šķērsots secīgi un tiek pārbaudīts katrs elements."
+				+"\n2 vai 3 atbildes var būt pareizas"
+				+ "\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas atbildes tad: a;b"
+				+ "\nAtbildes liekat alfabētu secībā!");
+		atbilde = atbilde.toLowerCase();
 		}while(!atbilde.equals("a;b") && !atbilde.equals("a;c") && !atbilde.equals("a;d") &&  !atbilde.equals("b;c") &&  !atbilde.equals("b;d") &&  !atbilde.equals("c;d") 
 				&&  !atbilde.equals("a;b;c") &&  !atbilde.equals("a;b;d") &&  !atbilde.equals("b;c;d") && !atbilde.equals("a;c;d"));
 		if(atbilde.equals("a;c")) punkti++;
 	}
 	public static void astotaisJautajums() {
 		do {
-			atbilde = JOptionPane.showInputDialog("Ir dots masīvs int A = {15,15,30,45,60,15}, kurā elementā atrodas skaitlis 15"
+			atbilde = JOptionPane.showInputDialog("8.Ir dots masīvs int A = {15,15,30,45,60,15}, kurā elementā atrodas skaitlis 15"
 					+ "\na)Skaitlis 15 atrodas 6 elementā"
 					+ "\nb)Skaitlis 15 atrodas 1 elementā"
 					+ "\nc)Skaitlis 15 atrodas 0 elementā"
 					+ "\nd)Skaitlis 15 atrodas 5 elementā"
 					+ "\n2 vai 3 atbildes var būt pareizas"
-					+ "\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas tad: a;b"
+					+ "\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas atbildes tad: a;b"
 					+ "\nAtbildes liekat alfabētu secībā!");
+			atbilde = atbilde.toLowerCase();
 		}while(!atbilde.equals("a;b") && !atbilde.equals("a;c") && !atbilde.equals("a;d") &&  !atbilde.equals("b;c") &&  !atbilde.equals("b;d") &&  !atbilde.equals("c;d") 
 				&&  !atbilde.equals("a;b;c") &&  !atbilde.equals("a;b;d") &&  !atbilde.equals("b;c;d") && !atbilde.equals("a;c;d"));
 		if(atbilde.equals("b;c;d")) punkti++;
@@ -99,8 +135,9 @@ public class Tests {
 					+ "\nc)Sadaliet masīvu mazākos apakšblokos, līdz tiek iegūti elementu pāri, un pēc tam apvienojiet tos tā, lai tie būtu sakārtoti."
 					+ "\nd)Izmanto iebūvēto funkciju."
 					+ "\n2 vai 3 atbildes var būt pareizas"
-					+ "\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas tad: a;b"
+					+ "\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas atbildes tad: a;b"
 					+ "\nAtbildes liekat alfabētu secībā!");
+			atbilde = atbilde.toLowerCase();
 		}while(!atbilde.equals("a;b") && !atbilde.equals("a;c") && !atbilde.equals("a;d") &&  !atbilde.equals("b;c") &&  !atbilde.equals("b;d") &&  !atbilde.equals("c;d") 
 				&&  !atbilde.equals("a;b;c") &&  !atbilde.equals("a;b;d") &&  !atbilde.equals("b;c;d") && !atbilde.equals("a;c;d"));
 		if(atbilde.equals("a;b")) punkti++;
@@ -113,11 +150,13 @@ public class Tests {
 					+ "\nc)Izmantojot mainīgos int sum un double vid un double n, kurš ir masivs.length un bez cikla"
 					+ "\nd)Izmantojot mainīgos int sum un double vid un double n, kurš ir masivs.length un while ciklu"
 					+ "\n2 vai 3 atbildes var būt pareizas"
-					+ "\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas tad: a;b"
+					+ "\nJums atbildes ir jāieraksta šādi: a;b;c, ja divas atbildes tad: a;b"
 					+ "\nAtbildes liekat alfabētu secībā!");
+			atbilde = atbilde.toLowerCase();
 		}while(!atbilde.equals("a;b") && !atbilde.equals("a;c") && !atbilde.equals("a;d") &&  !atbilde.equals("b;c") &&  !atbilde.equals("b;d") &&  !atbilde.equals("c;d") 
 				&&  !atbilde.equals("a;b;c") &&  !atbilde.equals("a;b;d") &&  !atbilde.equals("b;c;d") && !atbilde.equals("a;c;d"));
 		if(atbilde.equals("a;b")) punkti++;
+		System.out.println(punkti);
 	}
 	
 	public static void main(String[] args) {
